09662cb1e997b5d7d107efba3417282d3b02b701
arththakkar1 <143109571+arththakkar1@users.noreply.github.com>
2024-07-01 15:51:46 +0530

    first commit

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,33 +1,49 @@
-"use client"
-import MeetingRoom from "@/components/MeetingRoom";
-import MeetingSetup from "@/components/MeetingSetup";
-import { useUser } from "@clerk/nextjs";
-import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
-import React, { useState } from "react";
-import { useGetCallById } from "../../../../../hooks/useGetCallById";
-import Loader from "@/components/Loader";
+'use client';
 
-export default function page({ params:{id} }: { params: { id: string } }) {
-  
-  const {user, isLoaded}=useUser()
-  const [isSetupComplete, setIsSetupComplete] = useState(false)
-  const {call,isCallLoading}=useGetCallById(id)
+import { useState } from 'react';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { useParams } from 'next/navigation';
+import { Loader } from 'lucide-react';
 
-  if(!isLoaded||isCallLoading)return<Loader/>
+import { useGetCallById } from '../../../../../hooks/useGetCallById';
+import { Alert } from '@/components/ui/alert';
+import MeetingSetup from '@/components/MeetingSetup';
+import MeetingRoom from '@/components/MeetingRoom';
+
+const MeetingPage = () => {
+  const { id } = useParams();
+  const { isLoaded, user } = useUser();
+  const { call, isCallLoading } = useGetCallById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  if (!call) return (
+    <p className="text-center text-3xl font-bold text-white">
+      Call Not Found
+    </p>
+  );
+
+  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((ma: { user: { id: string; }; }) => ma.user.id === user.id));
+
+  if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
 
   return (
-    <main className="h-screan w-full">
-        <StreamCall call={call}>
-          <StreamTheme>
-            {!isSetupComplete?(
-              <MeetingSetup
-              setIsSetupComplete={setIsSetupComplete}
-              />
-            ):(
-              <MeetingRoom/>
-            )}
-          </StreamTheme>
-        </StreamCall>
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+
+        {!isSetupComplete ? (
+          <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+        ) : (
+          <MeetingRoom />
+        )}
+        </StreamTheme>
+      </StreamCall>
     </main>
   );
-}
+};
+
+export default MeetingPage;
--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,25 +1,22 @@
-"use client";
+'use client';
 
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import Image from "next/image";
-import Link from "next/link";
-import { SidebarLinks } from "../../data/sidebar";
-import { cn } from "@/lib/utils";
-import { usePathname } from "next/navigation";
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-function MobileNav() {
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { SidebarLinks } from '../../data/sidebar';
+import { cn } from '@/lib/utils';
+
+const MobileNav = () => {
   const pathname = usePathname();
+
   return (
     <section className="w-full max-w-[264px]">
       <Sheet>
         <SheetTrigger asChild>
           <Image
-            src="icons/hamburger.svg"
+            src="/icons/hamburger.svg"
             width={36}
             height={36}
             alt="hamburger icon"
@@ -32,38 +29,36 @@ function MobileNav() {
               src="/icons/logo.svg"
               width={32}
               height={32}
-              alt="Aloom Logo"
-              className="max-sm:size-10"
+              alt="yoom logo"
             />
-            <p className="text-[26px] font-extrabold text-white ">Aloom</p>
+            <p className="text-[26px] font-extrabold text-white">YOOM</p>
           </Link>
-          <div className="flex h-[calc(100vh-72px)] flex-col justify-normal overflow-y-auto">
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
-              <section className="flex h-full flex-col gap-6 pt-16 text-white">
-                {SidebarLinks.map((link) => {
-                  const isActive = pathname === link.route;
+              <section className=" flex h-full flex-col gap-6 pt-16 text-white">
+                {SidebarLinks.map((item) => {
+                  const isActive = pathname === item.route;
+
                   return (
-                    <SheetClose asChild>
-                    <Link
-                      href={link.route}
-                      key={link.label}
-                      className={cn(
-                        "flex gap-4 items-center p-4 rounded-lg w-full max-w-60",
-                        {
-                          "bg-blue-1": isActive,
-                        }
-                      )}
-                    >
-                      <Image
-                        src={link.imageUrl}
-                        alt={link.label}
-                        width={20}
-                        height={20}
-                      />
-                      <p className="font-semibold">
-                        {link.label}
-                      </p>
-                    </Link>
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn(
+                          'flex gap-4 items-center p-4 rounded-lg w-full max-w-60',
+                          {
+                            'bg-blue-1': isActive,
+                          }
+                        )}
+                      >
+                        <Image
+                          src={item.imageUrl}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                        />
+                        <p className="font-semibold">{item.label}</p>
+                      </Link>
                     </SheetClose>
                   );
                 })}
@@ -74,6 +69,6 @@ function MobileNav() {
       </Sheet>
     </section>
   );
-}
+};
 
 export default MobileNav;
